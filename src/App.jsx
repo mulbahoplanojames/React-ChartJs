e1e9c8adc249aa183a7a3e08b053ae25857ed6d5
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-07 16:24:58 +0200

    This is just a pratice exercise on the React CHart Js packege

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import BarChart from "./Components/Bar Chart/BarChart";
 import LineChart from "./Components/Line Chart/LineChart";
 
 const App = () => {
 	return (
 		<>
-			<div className='flex justify-center items-center  h-[100vh]'>
+			<div className='flex justify-center items-center gap-10  h-[100vh]'>
 				<LineChart />
+				<BarChart />
 			</div>
 		</>
 	);
--- src/Components/Bar Chart/BarChart.jsx
+++ src/Components/Bar Chart/BarChart.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Bar } from "react-chartjs-2";
+import { barData } from "../../Data/Data";
+
+import {
+	Chart as chartjs,
+	PointElement,
+	CategoryScale,
+	LinearScale,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+
+chartjs.register(
+	PointElement,
+	CategoryScale,
+	LinearScale,
+	Title,
+	Tooltip,
+	Legend
+);
 
 const BarChart = () => {
-	return <div>BarChart</div>;
+	return (
+		<>
+			<div className='w-[400px] h-[300px] rounded-md shadow-2xl border-2 border-red-600 overflow-hidden'>
+				<Bar data={barData} />;
+			</div>
+		</>
+	);
 };
 
 export default BarChart;
